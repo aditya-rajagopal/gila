b10f6f264fb0d2e0465629f59a2f671e8b6c16f9
Aditya Rajagopal <adi.raj465@gmail.com>
2025-12-05 15:21:38 -0800

    Ready. Set. Go.

---
+++ src/main.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+const gila = @import("gila");
+
+pub fn main() !void {
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    try stdout.print("{s}", .{gila.logo});
+
+    try stdout.flush(); // Don't forget to flush!
+}

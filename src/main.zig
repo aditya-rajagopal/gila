b520d4c409d3b8e66e5b5100cfe7557947589f2a
Aditya Rajagopal <adi.raj465@gmail.com>
2025-12-08 23:10:06 -0800

    wip: Added cli parser and basic init command

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,143 @@
 const std = @import("std");
+const assert = std.debug.assert;
+
 const gila = @import("gila");
+const DateTimeUTC = gila.DateTimeUTC;
+
+const flags = @import("flags.zig");
+const log = std.log.scoped(.gila);
+
+pub const std_options: std.Options = .{
+    .logFn = logFn,
+};
+
+pub fn logFn(
+    comptime level: std.log.Level,
+    comptime scope: @Type(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const level_text = comptime level.asText();
+    const scope_prefix = if (scope == .gila) ": " else "(" ++ @tagName(scope) ++ "): ";
+    const date_time = DateTimeUTC.now();
+
+    const writer = if (level != .err) blk: {
+        var stdout_writer = std.fs.File.stdout().writer(&.{});
+        const stdout = &stdout_writer.interface;
+        break :blk stdout;
+    } else blk: {
+        var stderr_writer = std.fs.File.stderr().writer(&.{});
+        const stderr = &stderr_writer.interface;
+        std.debug.lockStdErr();
+        break :blk stderr;
+    };
+
+    nosuspend {
+        date_time.format("", .{}, writer) catch return;
+        writer.print(" " ++ level_text ++ scope_prefix ++ format ++ "\n", args) catch return;
+    }
+    if (level == .err) {
+        std.debug.unlockStdErr();
+    }
+}
+
+const EnumTest = enum(u8) { a, b, c };
+
+const CLIArgs = union(enum) {
+    init: Init,
+
+    const Init = struct {
+        bare: bool = false,
+        positional: struct {
+            directory: ?[]const u8 = null,
+        },
+
+        pub const help =
+            \\Usage: gila init [--bare] [<directory>]
+            \\
+            \\Initializes a new GILA project in the current directory or the specified directory.
+            \\
+            \\Options:
+            \\  --bare  Creates a bare project without subfolders and tracking files.
+            \\  <directory>  The directory to initialize the project in. Defaults to the current directory.
+            \\
+        ;
+    };
+
+    pub const help =
+        \\Usage:
+        \\
+        \\    gila [-h | --help]
+        \\
+        \\    gila init [-h | --help] [--bare] [<directory>]
+        \\
+        \\Commands:
+        \\    init  Initializes a new GILA project in the current directory or the specified directory.
+        \\
+        \\Options:
+        \\    -h, --help
+        \\        Prints this help message.
+        \\
+    ;
+};
 
 pub fn main() !void {
-    var stdout_buffer: [4096]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+    var stack_space: [1024 * 1024]u8 = undefined;
+    var permanent_storage = std.heap.FixedBufferAllocator.init(stack_space[0 .. 1024 * 1024]);
 
-    try stdout.print("{s}", .{gila.logo});
-    try stdout.flush(); // Don't forget to flush!
+    const permanent_allocator = permanent_storage.allocator();
+    var args = try std.process.argsWithAllocator(permanent_allocator);
+
+    // var writer = std.fs.File.stdout().writer(&.{}).interface;
+    // writer.print("{s}", .{gila.logo}) catch unreachable;
+    // writer.print("GILA SPEC v0.0.1\n", .{}) catch unreachable;
+    const cli = flags.parseArgs(&args, CLIArgs);
+
+    switch (cli) {
+        .init => |init| {
+            const buffer: []u8 = permanent_allocator.alloc(u8, std.fs.max_path_bytes) catch unreachable;
+
+            const current_dir: []const u8 = try std.process.getCwd(buffer);
+            var dir = try std.fs.openDirAbsolute(current_dir, .{ .iterate = true });
+            defer dir.close();
+
+            dir.makeDir(gila.dir_name) catch |err| {
+                switch (err) {
+                    error.PathAlreadyExists => {
+                        log.err("Reinitializing existing GILA project: {s}/{s}", .{ current_dir, gila.dir_name });
+                        return;
+                    },
+                    else => |e| {
+                        log.err("Failed to initialize GILA project: {s}", .{@errorName(e)});
+                        return;
+                    },
+                }
+            };
+            log.info("Initialized GILA project: {s}/{s}", .{ current_dir, gila.dir_name });
+            if (init.bare) {
+                return;
+            }
+            log.info("TODO: init sub folders and tracking files", .{});
+            return;
+        },
+    }
+
+    const buffer: []u8 = permanent_allocator.alloc(u8, std.fs.max_path_bytes) catch unreachable;
+    var current_dir: []const u8 = try std.process.getCwd(buffer);
+
+    outter_loop: for (0..128) |_| {
+        var dir = try std.fs.openDirAbsolute(current_dir, .{ .iterate = true });
+        defer dir.close();
+
+        var dir_walker = dir.iterateAssumeFirstIteration();
+        while (try dir_walker.next()) |entry| {
+            if (entry.kind == .directory) {
+                if (std.mem.eql(u8, entry.name, gila.dir_name)) {
+                    log.info("Found .gila directory: {s}/{s}\n", .{ current_dir, entry.name });
+                    break :outter_loop;
+                }
+            }
+        }
+        current_dir = std.fs.path.dirname(current_dir) orelse break;
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,97 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
+pub const dir_name = ".gila";
+
+pub const description_file_name = "description.md";
+pub const comments_file_name = "comments.md";
+
+pub const Status = union(enum(u8)) {
+    todo,
+    in_progress,
+    done,
+    cancelled,
+    waiting: ?[]const TaskId,
+};
+
+pub const TaskId = struct {
+    date_time: DateTimeUTC,
+    user_name: []const u8,
+};
+
+// https://github.com/tigerbeetle/tigerbeetle/blob/16d62f0ce7d4ef3db58714c9b7a0c46480c19bc3/src/stdx.zig#L985
+pub const DateTimeUTC = packed struct(u64) {
+    year: u16,
+    month: u8,
+    day: u8,
+    hour: u8,
+    minute: u8,
+    second: u6,
+    millisecond: u10,
+
+    pub fn now() DateTimeUTC {
+        const timestamp_ms = std.time.milliTimestamp();
+        assert(timestamp_ms > 0);
+        return DateTimeUTC.from_timestamp_ms(@intCast(timestamp_ms));
+    }
+
+    pub fn from_timestamp_ms(timestamp_ms: u64) DateTimeUTC {
+        const epoch_seconds = std.time.epoch.EpochSeconds{ .secs = @divTrunc(timestamp_ms, 1000) };
+        const year_day = epoch_seconds.getEpochDay().calculateYearDay();
+        const month_day = year_day.calculateMonthDay();
+        const time = epoch_seconds.getDaySeconds();
+
+        return DateTimeUTC{
+            .year = year_day.year,
+            .month = month_day.month.numeric(),
+            .day = month_day.day_index + 1,
+            .hour = time.getHoursIntoDay(),
+            .minute = time.getMinutesIntoHour(),
+            .second = time.getSecondsIntoMinute(),
+            .millisecond = @intCast(@mod(timestamp_ms, 1000)),
+        };
+    }
+
+    pub fn format(
+        datetime: DateTimeUTC,
+        comptime fmt: []const u8,
+        options: std.fmt.Options,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+        try writer.print("{d:0>4}-{d:0>2}-{d:0>2} {d:0>2}:{d:0>2}:{d:0>2}.{d:0>3}Z", .{
+            datetime.year,
+            datetime.month,
+            datetime.day,
+            datetime.hour,
+            datetime.minute,
+            datetime.second,
+            datetime.millisecond,
+        });
+    }
+
+    pub fn dateAsNumber(self: DateTimeUTC) u32 {
+        return @as(u32, self.year) * 10000 + @as(u32, self.month) * 100 + @as(u32, self.day);
+    }
+
+    pub fn timeAsNumber(self: DateTimeUTC) u32 {
+        return @as(u32, self.hour) * 10000 + @as(u32, self.minute) * 100 + @as(u32, self.second);
+    }
+};
+
+pub const Priority = union(enum) {
+    low: u8,
+    medium: u8,
+    high: u8,
+    urgent: u8,
+};
+
+pub const Tag = []const u8;
+
 pub const logo =
     \\⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠏⣀⡤⠀⠀⠀⠀⠀
-    \\⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠊⠀⠐⡪⠴⠃⠀⠀⠀⠀
+    \\⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀ ⠀⠊⠀⠐⡪⠴⠃⠀⠀⠀⠀
     \\⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠊⠀⠀⣀⣠⠤⢴⣊⠉⠁⠀⠀⠀⠀
     \\⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⣀⣀⣾⣷⣶⠋⠉⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀
     \\⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⠿⣋⡉⠿⠿⠇⢰⣶⣦⣭⡍⠙⢶⣦⡤⣤⣴⣦⣤⡀⠀⠀⠀⠀
